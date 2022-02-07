7d2a650ca351a06cdc952095817cb9ea731627c2
Moliner Eloi <molinee2@login3.triton.aalto.fi>
2022-02-07 18:22:09 +0200

    aadding

---
+++ train_bwe.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+##SBATCH  --time=00:59:59
+#SBATCH  --time=2-23:59:59
+##SBATCH  --time=03:59:59
+#SBATCH --mem=10G
+#SBATCH --cpus-per-task=4
+#SBATCH --job-name=bwe_progressive_half_size
+#SBATCH  --gres=gpu:a100:1
+##SBATCH  --gres=gpu:1 --constraint=volta
+##SBATCH  --gres=gpu:1 
+##SBATCH  --gres=gpu:1 --exclude=gpu[20-27]
+##SBATCH  --gres=gpu:1 --exclude=gpu[20-27]
+##SBATCH  --gres=gpu:4 
+##SBATCH  --gres=gpu:3 --constraint=volta
+##SBATCH --output=/scratch/work/%u/unet_dir/unet_historical_music/experiments/23062021_metrics_mae/training_mse%j.out
+#SBATCH --output=/scratch/work/%u/unet_dir/bwe_historical_recordings/experiments/%a_training_%j.out
+#SBATCH --array=[2]
+
+# ..mem=80G
+module load anaconda 
+source activate /scratch/work/molinee2/conda_envs/bwe_test
+export TORCH_USE_RTLD_GLOBAL=YES
+#experiment number
+n=3
+#n=2
+#iteration=`sed -n "${n} p" iteration_parameters_bwe.txt`      # Get n-th line (2-indexed) of the file
+
+
+PATH_EXPERIMENT=experiments_bwe/${n}
+mkdir $PATH_EXPERIMENT
+
+
+python train_bwe.py path_experiment="$PATH_EXPERIMENT"   #override here the desired parameters using hydra
+#python train_NLD.py path_experiment="$PATH_EXPERIMENT"  $iteration 
---
+++ train_denoiser.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+##SBATCH  --time=00:59:59
+#SBATCH  --time=2-23:59:59
+#SBATCH --mem=10G
+#SBATCH --cpus-per-task=10
+#SBATCH --job-name=training_array_metrics
+##SBATCH  --gres=gpu:a100:1
+#SBATCH  --gres=gpu:1 --constraint=volta
+##SBATCH  --gres=gpu:4 --constraint=volta
+##SBATCH  --gres=gpu:3 --constraint=volta
+##SBATCH --output=/scratch/work/%u/unet_dir/unet_historical_music/experiments/23062021_metrics_mae/training_mse%j.out
+#SBATCH --output=/scratch/work/%u/unet_dir/denoising_pytorch/experiments/%a_training_%j.out
+#SBATCH --array=[21,22,23]
+
+# ..mem=80G
+module load anaconda 
+source activate /scratch/work/molinee2/conda_envs/bwe_test
+
+n=$SLURM_ARRAY_TASK_ID
+n=3
+iteration=`sed -n "${n} p" iteration_parameters_denoiser.txt`      # Get n-th line (2-indexed) of the file
+
+PATH_EXPERIMENT=experiments_denoiser/${n}
+mkdir $PATH_EXPERIMENT
+
+python  train_denoiser.py path_experiment="$PATH_EXPERIMENT" $iteration  epochs=150  num_workers=10
